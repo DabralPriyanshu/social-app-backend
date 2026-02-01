7a4adddc63693d98f699ab997e766b99145b242a
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-01 13:44:31 +0530

    Added middleware to validate register user api  req body

---
+++ src/middlewares/user.middleware.js
@@ -0,0 +1,29 @@
+const Errors = require("../utils/errors/index");
+
+const validateCreateUserRequest = (req, res, next) => {
+  try {
+    if (!req.body?.email) {
+      const err = new Errors.BadRequestError(
+        "Email is not present in the incoming request body",
+      );
+      return res.status(err.statusCode).json(err);
+    }
+    if (!req.body?.password) {
+      const err = new Errors.BadRequestError(
+        "Password is not present in the incoming request body",
+      );
+      return res.status(err.statusCode).json(err);
+    }
+    if (!req.body?.username) {
+      const err = new Errors.BadRequestError(
+        "Username is not present in the incoming request body",
+      );
+      return res.status(err.statusCode).json(err);
+    }
+    next();
+  } catch (error) {
+    const err = new Errors.ServerError();
+    return res.status(err.statusCode).json(err);
+  }
+};
+module.exports = { validateCreateUserRequest };
--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const userMiddlewares = require("../../middlewares/user.middleware");
 const userController = require("../../controllers/user.controller");
 const userRouter = express.Router();
-userRouter.post("/register", userController.create);
+userRouter.post(
+  "/register",
+  userMiddlewares.validateCreateUserRequest,
+  userController.create,
+);
 module.exports = userRouter;
