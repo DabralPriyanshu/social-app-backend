9c07353f46b72fd718495050e921ffab66d7cdec
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-03 18:42:15 +0530

    Added frontend deployed url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { PORT } = require("./config/server.config");
+const { PORT, FRONTEND_URL } = require("./config/server.config");
 const connectToDB = require("./config/db.config");
 const cookieParser = require("cookie-parser");
 const apiRoutes = require("./routes/index");
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //config cors to avoid cors error so that frontend can request backend and backend have idea about from where the request is coming
+app.use(cors({ origin: FRONTEND_URL, credentials: true })); //config cors to avoid cors error so that frontend can request backend and backend have idea about from where the request is coming
 app.use("/api", apiRoutes); //if after server url /api will be present in the endpoints the request will be passed to apiRoutes
 
 app.get("/", (req, res) => {
